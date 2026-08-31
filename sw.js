8b0f1150ae9cee4f2caa57ba66a7fe88d3ca0d83
lqcin <hekim.lacin@hotmail.com>
2026-08-31 16:53:40 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Veri (Firebase/Firestore) zaten IndexedDB ile offline çalışıyor,
 // bu SW sadece sayfa dosyalarını (HTML, fontlar) önbelleğe alır.
 
-const CACHE_ADI = 'swisshouse-v30-payment-sync'; // belirgin bugün + güvenli rezervasyon fiyat/komisyoncu akışı
+const CACHE_ADI = 'swisshouse-v31-past-reservation-reconcile'; // belirgin bugün + güvenli rezervasyon fiyat/komisyoncu akışı
 const ONBELLEKLENECEKLER = [
   '/swisshouse/resepsiyon.html',
   '/swisshouse/komisyoncu.html',
